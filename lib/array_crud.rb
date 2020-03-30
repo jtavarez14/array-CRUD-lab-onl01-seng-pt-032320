e0528af24a8ef3f6f3cc7e4cf791f759c1154d7f
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-03-30 22:02:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index = ["array", "index_number", "am"]
-  retrieve_element_from_index.index(2)
+  retrieve_element_from_index.index [2]
 end
 
 def retrieve_first_element_from_array(array)
